24ede39da666079e613233f431fc73fdb4802e8b
Marija Nikolova <Marija.Nikolova@netcetera.com>
2021-09-24 10:58:34 +0200

    Added h2 in-memory database configuration.

--- src/main/java/com/finki/websavings/WebSavingsApplication.java
+++ src/main/java/com/finki/websavings/WebSavingsApplication.java
@@ -2,9 +2,8 @@ package com.finki.websavings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class WebSavingsApplication {
 
 	public static void main(String[] args) {
