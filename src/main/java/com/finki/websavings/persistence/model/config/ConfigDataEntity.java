512e58d6ec6fe9d106dda1ae0095cc8016968350
Marija Nikolova <Marija.Nikolova@netcetera.com>
2021-09-24 18:22:19 +0200

    Fixed sql issue with Config data.

--- src/main/java/com/finki/websavings/persistence/model/config/ConfigDataEntity.java
+++ src/main/java/com/finki/websavings/persistence/model/config/ConfigDataEntity.java
@@ -1,8 +1,10 @@
 package com.finki.websavings.persistence.model.config;
 
 import com.finki.websavings.persistence.model.customer.CustomerEntity;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CascadeType;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +17,8 @@ import java.io.Serializable;
 /**
  * The entity for the Config data model.
  */
+@Getter
+@Setter
 @Entity
 @org.hibernate.annotations.Proxy(lazy=false)
 @Table(name="CONFIG_DATA")
@@ -34,43 +38,8 @@ public class ConfigDataEntity implements Serializable {
 	@Column(name="language", nullable=true, length=255)
 	private String language;
 	
-	@OneToOne(cascade = CascadeType.MERGE)
+	@OneToOne
+	@org.hibernate.annotations.Cascade({CascadeType.LOCK})
 	@JoinColumn(name = "customerId", referencedColumnName = "id")
 	private CustomerEntity customer;
-
-	public void setNumberOfYears(Integer value) {
-		this.numberOfYears = value;
-	}
-	
-	public Integer getNumberOfYears() {
-		return numberOfYears;
-	}
-	
-	public void setLanguage(String value) {
-		this.language = value;
-	}
-	
-	public String getLanguage() {
-		return language;
-	}
-	
-	private void setId(int value) {
-		this.id = value;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setID(Integer ID) {
-		this.id = ID;
-	}
-
-	public CustomerEntity getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(CustomerEntity customer) {
-		this.customer = customer;
-	}
 }
